fde21288c1518d6da22aba833cc686b1a176cb4c
Thai Son Dang <thaisondang.bk57@gmail.com>
2018-11-24 00:44:23 +0100

    configured Gmail

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
+const nodemailer = require("nodemailer");
 
 ///////////////////////////////////
 const app = express();
 const port = process.env.PORT || 3000;
+
+////////////////////////////////// GMAIL configure
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  secure: true,
+  port: 465,
+  auth: {
+    user: process.env.FROM_EMAIL,
+    pass: process.env.PASS
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
 
 /////////////////////////////////// set VIEWS and VIEW ENGINE
 app.set("view engine", "ejs");
@@ -17,6 +34,38 @@ app.use(express.static(path.join(__dirname, "public")));
 /////////////////////////////////// ROUTES
 app.route("/").get((req, res) => {
   res.render("index");
+});
+
+app.post("/new-reservation", (req, res) => {
+  const { name, email, seat, time, date, note } = req.body;
+
+  const option = {
+    from: `RESERVATION ${name} <${email}`,
+    to: process.env.TO_EMAIL,
+    subject: name,
+    html: `
+        <p><b>NAME: ${name}</b></p>  
+        <p><b>EMAIL: ${email}</b></p>
+        <p><b>TIME: ${time}</b></p>
+        <p><b>DATE: ${date}</b></p>
+        <p><b>SEATS: ${seat}</b></p>
+        <p><b>NOTE: ${note || "empty"}</b></p>
+        `
+  };
+
+  transporter.sendMail(option, (err, info) => {
+    if (err) {
+      res.send({
+        status: "ERROR",
+        message: "Something went wrong. Your reservation failed."
+      });
+    } else {
+      res.send({
+        status: "OK",
+        message: "Thank you for your reservation!"
+      });
+    }
+  });
 });
 
 ///////////////////////////////////  LISTEN TO PORT
